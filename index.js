54efd75a5aa8e4d2b2167b33028be85bba38c267
deepesh Nagarkoti <deepesh.nagarkoti@gmail.com>
2019-07-30 11:45:52 -0700

    adding required folders to jwt

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
-const express = require('express')
-const morgan = require('morgan')
-const mongoose = require( 'mongoose')
-const express = require('express')
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+
+mongoose.connect('mongodb://localhost:auth/auth', {useNewUrlParser: true, useCreateIndex: true});
+
+app.use(morgan('combined'));
+app.use(express.json);
+app.use(express.urlencoded({extended: true}));
+app.use(cors());
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+}
+
+const routes = require('./routes');
+
+app.get('/', (req, res) => res.send(1))
+app.use(routes);
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
